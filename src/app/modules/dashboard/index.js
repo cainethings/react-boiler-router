dd223b6199515203517eeacec2f8d28d307f2a12
Caine Daniel <admin@cainethings.com>
2024-03-05 10:59:53 +0530

    Initial Setup

---
+++ src/app/modules/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './dashboard.scss';
+
+const Main = () => {
+  return (
+    <div className='module-dashboard'>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './app/library/styles/master.scss';
 import reportWebVitals from './reportWebVitals';
 
+/* Modules */
+import Dashboard from './app/modules/dashboard';
+
+const PageNotFound = () => {
+  return (
+    <h1>Page Not Found</h1>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
